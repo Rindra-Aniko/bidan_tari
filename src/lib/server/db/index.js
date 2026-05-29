1812bbcbfd7779fdd4a424f2129ebd0d0711eaa2
Rindra-Aniko <rindra.aniko@gmail.com>
2026-05-29 16:08:22 +0700

    perbaikan database

--- src/lib/server/db/index.js
+++ src/lib/server/db/index.js
@@ -2,15 +2,19 @@ import { drizzle } from 'drizzle-orm/libsql';
 import { createClient } from '@libsql/client';
 import * as schema from './schema';
 import { env } from '$env/dynamic/private';
+import { building } from '$app/environment';
 
 const url = env.DATABASE_URL || env.TURSO_CONNECTION_URL;
 const authToken = env.TURSO_AUTH_TOKEN;
 
-if (!url) throw new Error('Database Connection URL is not set');
+if (!url && !building) {
+	throw new Error('Database Connection URL is not set');
+}
 
-const client = createClient({
+const client = !building && url ? createClient({
 	url,
 	authToken: url.startsWith('file:') ? undefined : authToken
-});
+}) : null;
 
-export const db = drizzle(client, { schema });
+export const db = client ? drizzle(client, { schema }) : {};
+
